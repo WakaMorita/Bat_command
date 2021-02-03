2bde3eee085812730b3f746ebfbc809b1e976ff2
WakaMorita <waka_morita_business@yahoo.co.jp>
2021-02-03 23:46:59 +0900

    20210203 フォルダコピー処理他

--- ファイル移動他.bat
+++ ファイル移動他.bat
@@ -1,11 +1,21 @@
+rem 日本語のフォルダ名でも対応できるように文字コードを変更する
+chcp 65001
+
 rem ファイルのコピー
 copy "C:\〇\〇\〇\〇\無題1.txt" "C:\〇\〇\〇\無題1.txt"
 
+rem フォルダのコピー /sは空のディレクトリを自動的に除外する
+robocopy %USERPROFILE%\Desktop C:\new /s
+
 rem ショートカットのコピー　.lnkの拡張子が必要!
 copy "C:\〇\〇\〇\〇\無題1.txt - ショートカット.lnk" "C:\〇\〇\〇\無題1.txt - ショートカット.lnk"
+
+rem 親バッチプログラムを停止せずに別のバッチプログラムを呼び出す
+call checknew
 
 rem 管理者権限で実行
 powershell start-process "C:\WINDOWS\system32\notepad.exe" -verb runas
 
 rem 一時停止
 PAUSE
+
---
+++ 資格情報制御.bat
@@ -0,0 +1,4 @@
+rem targetname:コンピューターまたはドメイン名を識別するのパラメーター
+rem username:ユーザー名orアカウント名
+rem password:パスワード
+cmdkey /add targetname /user:username /pass:password
